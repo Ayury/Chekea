807343f1dad981989180fb330671cfc46a2348ac
Ayury <88218886+Ayury@users.noreply.github.com>
2023-07-20 11:55:06 -0500

    letras de la empresa

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -14,18 +14,20 @@ export const Header = () => {
   return (
     <header className="header">
     <div className=" logo"><img src={logo} alt=""/></div>
-    <div className=" nombreEmpresa degradado-verde"><h1 className="nombre-logo">Chekea</h1><p className="slogan">Nunca Compres a ciegas, chekea</p></div>
+    <div className=" nombreEmpresa "><h1 className="nombre-logo">Chekea</h1><p className="slogan">Nunca Compres a ciegas, chekea</p></div>
     <nav className="navPrincipal">
         <a href="#">Inicio</a>
         <a href="#">Autos</a>
-        <a href="#">Venta</a>
-        <a href="#">Taller</a>
+        <a href="#">Vender</a>
+        <a href="#">Talleres</a>
        
       </nav>
       <div className="social-icons">
         Redes Sociales
-        <button onClick={CerrarSesion}>Cerrar Sesion</button> 
+         
       </div>
+      <div className='sesion'>  <button onClick={CerrarSesion}>Cerrar Sesion</button></div>
+    
    {/*   //<nav className="nav">
         <a href="#">Iniciar Sesion</a>
          
