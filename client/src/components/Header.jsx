4f00571940f35a9dc73ac32309cfdcb0a8769e74
Ayury <88218886+Ayury@users.noreply.github.com>
2023-07-20 11:24:18 -0500

    cambios en header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useChekea } from '../context/ChekeaContext';
+import logo from '../assets/logo.png';
+
 
 export const Header = () => {
 
@@ -11,9 +13,9 @@ export const Header = () => {
 
   return (
     <header className="header">
-    <div className=" logo">Logo<img src="{logo-empresa.png}" alt=""/></div>
+    <div className=" logo"><img src={logo} alt=""/></div>
     <div className=" nombreEmpresa degradado-verde"><h1 className="nombre-logo">Chekea</h1><p className="slogan">Nunca Compres a ciegas, chekea</p></div>
-    <nav className="nav">
+    <nav className="navPrincipal">
         <a href="#">Inicio</a>
         <a href="#">Autos</a>
         <a href="#">Venta</a>
@@ -22,12 +24,13 @@ export const Header = () => {
       </nav>
       <div className="social-icons">
         Redes Sociales
+        <button onClick={CerrarSesion}>Cerrar Sesion</button> 
       </div>
-      <nav className="nav">
+   {/*   //<nav className="nav">
         <a href="#">Iniciar Sesion</a>
-        <button onClick={CerrarSesion}>Cerrar Sesion</button>  
-      </nav>
-    
+         
+      </nav>//
+     */}
     
   </header>
 
